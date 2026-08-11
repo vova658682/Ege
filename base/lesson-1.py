25488dbcae09c95176b700f1077c72426d706d51
vova <vovajach@gmail.com>
2026-08-11 17:51:51 +0200

    пнятие переменной

---
+++ base/lesson-1.py
@@ -0,0 +1,13 @@
+#Переменная
+num = 16
+#= - оператор присваивания
+
+
+# Натации- правила оформления названий переменных
+# snake_case - client_age
+# camelCase - clientAge, ageOfClient
+# PascalCase - ClientAge, AgeOfClient
+
+
+# Правила алфавита для названия переменных
+# Можно использовать: a-z, A-Z, 0-9, _
