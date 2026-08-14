60f6c5dbbb5aacb4db7bc5f41ba73a2ac4ec794b
vova <vovajach@gmail.com>
2026-08-14 17:35:35 +0200

    преобразование типов данных

---
+++ base/lesson 3.py
@@ -0,0 +1,10 @@
+# преобразование типов данных
+# int - float, str, bool
+# float - int, str , bool
+# str - int , float , list , tuple , set , bool
+# list - str, tuple, set, bool
+# tuple - str, list, set, boll
+# set - str, list, tuple, bool
+# bool - int, float, str
+test = 5
+print(float(test))
