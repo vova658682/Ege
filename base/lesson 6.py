f07eab3d9fa7cf1713a66e0199ed00cd309b99fe
ROMAN <vovajach@gmail.com>
2026-08-18 20:53:42 +0200

    Модули и библиотеке

---
+++ base/lesson 6.py
@@ -0,0 +1,26 @@
+#Модули и библиотеки
+# Импорт всей библиотеке
+import math
+
+# Импорт всей библиотеке через псевдоним
+import math as m
+
+# Импорт одной команды
+from math import floor
+
+# Импорт одној команды через псевдоним
+from math import floor as f
+
+# Импорт нескольких команд
+from math import log2,log10
+
+# Импорт нескольких команд через псевдоним
+from math import log2 as l2,log10 as l10
+
+# Импорт всех команд
+from math import *
+num = 10.5
+print (math.ceil(num))#Округление в болғшуө сторону
+print (floor(num))#Округление в менғшуө строну
+print (log2(16))
+print (log10(100))
