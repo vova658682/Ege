1347e45bc0c3b6ddd1bb6707706aeffe49fafe77
vova <vovajach@gmail.com>
2026-08-25 17:59:28 +0200

    циклы for

---
+++ base/lesson 10.py
@@ -0,0 +1,40 @@
+# Цикл while
+
+# Нахождение муссы чифр числа любой длины
+
+#num = 123 # 1
+#summ = 0 # 3+2+1+0
+#while num > 0:
+#    summ += num % 10# summ = 0 + 123 % 10
+#    num //= 10 #
+#print(summ)
+
+
+
+# Цикл for
+
+# rang(n, m , k)-создет числовой диапозон от n до m  с шагои k
+print(*range(5, 13))# от 5 до не включительно
+print(*range(10))# от 10 до 10 н включительно
+print(*range(4,10,2))# от 4 до 10 не вклб с шагом 2
+
+
+for i in range(-1, -14, -3):    # (от -1 до -14 вкл)
+    print(i)
+
+data = ['User1', 'User2', 'User3']
+for i in data:
+    print(i)
+
+
+
+
+
+
+
+#for i in range(5):
+
+
+
+
+
