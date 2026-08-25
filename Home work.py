79b9adc7fd760f074621151ab09a9cbd4ac4828c
ROMAN <vovajach@gmail.com>
2026-08-25 14:06:59 +0200

    дз

--- Home work.py
+++ Home work.py
@@ -17,26 +17,56 @@
 import math
 
 
-a = float(input("Введите коэффициент a: "))
-b = float(input("Введите коэффициент b: "))
-c = float(input("Введите коэффициент c: "))
+#a = float(input("Введите коэффициент a: "))
+#b = float(input("Введите коэффициент b: "))
+#c = float(input("Введите коэффициент c: "))
 
 
-if a == 0:
-    print("Неможет быть равен 0 ")
-else:
-    discriminant = b ** 2 - 4 * a * c
+#if a == 0:
+#    print("Неможет быть равен 0 ")
+#else:
+#    discriminant = b ** 2 - 4 * a * c
+#
+#    if discriminant > 0:
+#
+#        x1 = (-b + math.sqrt(discriminant)) / (2 * a)
+#        x2 = (-b - math.sqrt(discriminant)) / (2 * a)
+#        print(f"x1 = {x1}, x2 = {x2}")
+#    elif discriminant == 0:
+#        x = -b / (2 * a)
+#        print(f"x = {x}")
+#    else:
+#        print("нет решений ")
+#    ###############################################################
+#x = int(input('Введите число:'))
+#if x > 0:
+#    x += 1
+#    print(x)
+#elif  x <= 0:
+#    x -= 1
+#    print(x)
+###################################################################
+#import random
+#from unittest import result
+#
+#num = random.randint(0,100)
+#if num % 3 == 0:
+#    result = num // 3
+#    print(f"Число {num} делится на 3. Частное: {result}")
+#else:
+#    result = num * 2
+#    print(f"Число {num} умножается на 2. Частное: {result}")
+###################################################################
+#x = float(input("Введите значение x: "))
+#if x > 0:
+#    y = 2 * x + 5
+#elif x == 0:
+#    y = x
+#else:
+#    y = x ** 2 - 1
+#print(f"При x = {x} Значение  y = {y}")
+#
 
-    if discriminant > 0:
-
-        x1 = (-b + math.sqrt(discriminant)) / (2 * a)
-        x2 = (-b - math.sqrt(discriminant)) / (2 * a)
-        print(f"x1 = {x1}, x2 = {x2}")
-    elif discriminant == 0:
-        x = -b / (2 * a)
-        print(f"x = {x}")
-    else:
-        print("нет решений ")
 
 
 
